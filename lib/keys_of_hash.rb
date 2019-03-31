0b6aac3064d954cfbe55338ca01d8714fc6629db
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-31 20:31:20 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,11 +4,10 @@ class Hash
   def keys_of(*arguments)
     new_array = []
     
-    :animals.each{|key, val|
+    self.each{|key, val|
       if val == arguments
         new_array.push(key)  
       end 
     }
-    binding.pry
   end
 end
