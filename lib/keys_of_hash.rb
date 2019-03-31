99e133393f81d58299600b3abdd9e95da5b4686d
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-31 20:17:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,4 @@
 class Hash
   def keys_of(*arguments)
     new_array = []
-    
-    hash.each {|key, val|
-      if val == arguments
-        new_array.push(key)
-      end 
-    }
-    new_array
-    end 
 end
