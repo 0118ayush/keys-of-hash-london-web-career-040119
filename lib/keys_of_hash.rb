32c557a7e3b83dd5d2c03c06eb86079877c56a12
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-31 20:36:39 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,11 +3,12 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     
+    new_array = []
     self.each{|key, val|
       if val == arguments
-        keys_array = self.keys  
+        new_array.push(self.keys)  
       end 
     }
-    keys_array
+    new_array
   end
 end
