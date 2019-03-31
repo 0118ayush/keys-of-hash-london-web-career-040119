05e5bdeaf36c1650eea3788c66670746bab2b1f0
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-31 21:00:27 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
     
     new_array = []
     self.each{|key, val|
-      if val == arguments
+      if self.include?(arguments)
         new_array.push(key)  
       end 
       #binding.pry
